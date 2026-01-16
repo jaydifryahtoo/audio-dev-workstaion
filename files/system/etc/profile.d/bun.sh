741b2f1c6eb58907c3cc52489ed2a816bed713c3
jaydifryah <47575592+jaydifryah@users.noreply.github.com>
2026-01-15 22:50:19 -0800

    major refactor

---
+++ files/system/etc/profile.d/bun.sh
@@ -0,0 +1,4 @@
+
+# These environment variables are the most reliable way to set global install paths for bun
+export BUN_INSTALL_GLOBAL_DIR="/var/lib/bun/install/global"
+export BUN_INSTALL_BIN="/usr/local/bin"
